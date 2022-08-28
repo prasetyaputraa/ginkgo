127c91ae009be2df09e80da3d5b1747e58b80002
Prasetya Putra <prasetyaputraa@gmail.com>
2022-08-28 17:37:25 +0800

    add applications providers and configuration

--- ginkgo.go
+++ ginkgo.go
@@ -1,4 +1,16 @@
 package ginkgo
 
-func main() {
+import (
+	conf "github.com/prasetyaputraa/ginkgo/conf"
+	providerApplications "github.com/prasetyaputraa/ginkgo/providers/applications"
+)
+
+type Providers struct {
+	applications func(conf conf.Configuration)
+}
+
+func GetProviders() Providers {
+	return Providers{
+		applications: providerApplications.Provide,
+	}
 }
